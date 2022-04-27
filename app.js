63412f27e323f8af00737aa95ba829fe71bd6aff
kathrynzaro <zarokathryn@gmail.com>
2022-04-27 11:33:58 -0700

    item 2 cheese

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+// grab element
 const soda = document.getElementById('soda');
+const cheese = document.getElementById('cheese');
 
-
+// add event listener
 soda.addEventListener('click', () => {
     soda.classList.toggle('picked');
+})
+
+cheese.addEventListener('click', () => {
+    cheese.classList.toggle('picked');
 })
