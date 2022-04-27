4145e8c6f7e7838f3a2f9eade5ba7affa1ec9787
kathrynzaro <zarokathryn@gmail.com>
2022-04-27 11:43:38 -0700

    item 5 watermelon

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const soda = document.getElementById('soda');
 const cheese = document.getElementById('cheese');
 const grapes = document.getElementById('grapes');
 const bread = document.getElementById('bread');
+const watermelon = document.getElementById('watermelon');
 
 // add event listener
 soda.addEventListener('click', () => {
@@ -19,4 +20,8 @@ grapes.addEventListener('click', () => {
 
 bread.addEventListener('click', () => {
     bread.classList.toggle('picked');
+});
+
+watermelon.addEventListener('click', () => {
+    watermelon.classList.toggle('picked');
 });
